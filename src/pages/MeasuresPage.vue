2df61da97ce80fcf5e318c3514f68fb7b93ea251
Chappuis Flavien <75837575+Flavien-dev@users.noreply.github.com>
2023-03-15 11:05:55 +0100

    j'ai commenté le code de la page des mesures

--- src/pages/MeasuresPage.vue
+++ src/pages/MeasuresPage.vue
@@ -18,21 +18,31 @@
 </template>
 
 <script>
+// importation des actions et des composants
 import { mapActions } from 'vuex'
 import Measure from 'components/Measure.vue'
 
 export default {
+  // nom de la page
   name: 'MeasuresPage.vue',
+  // composant de la page
   components: { Measure },
   computed: {
+    /**
+     * retourne toutes les mesures des capteurs
+     * @returns listes des mesures
+     */
     measures () {
       return this.$store.getters['measures/getMeasures']
     }
   },
+  // appel des actions
   methods: {
+    // appel de l'action qui renvoie les mesures stockées dans l'API
     ...mapActions('measures', ['getMeasuresApi'])
   },
   mounted () {
+    // renvoie les mesures de l'API
     this.getMeasuresApi()
   }
 }
