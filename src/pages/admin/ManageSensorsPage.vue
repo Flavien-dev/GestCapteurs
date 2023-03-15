8ce3cc2a2b633dce16cac15069ae0a16a2eade09
Chappuis Flavien <75837575+Flavien-dev@users.noreply.github.com>
2023-03-15 08:36:17 +0100

    j'ai commenté le code de la page de managment des capteurs

--- src/pages/admin/ManageSensorsPage.vue
+++ src/pages/admin/ManageSensorsPage.vue
@@ -32,24 +32,33 @@ import { mapActions } from 'vuex'
 import Sensor from 'components/admin/AdminSensors.vue'
 
 export default {
+  // nom de la page
   name: 'ManageSensorsPage.vue',
+  // composants utilisés dans la page
   components: {
+    // capteur
     Sensor,
+    // formulaire d'ajout de capteur
     'form-new-sensor': require('components/action/capteur/creeCapteur.vue').default
   },
+  // données de la page
   data () {
     return {
+      // permet de savoir si l'administrateur veut ajouter un nouveau capteur
       afficheFormNewSensor: false,
+      // permet de savoir si l'administrateur veut modifier un capteur
       afficheFormModifSensor: false
     }
   },
   computed: {
+    // retourne les capteurs stockés dans l'API
     sensors () {
       return this.$store.getters['sensors/getSensors']
     }
   },
+  // appel des méthodes
   methods: {
-    // Donne accès à l'action getClientsApi() du magasin Clients
+    // Donne accès à l'action getSensorsApi() du magasin Sensors
     ...mapActions('sensors', ['getSensorsApi'])
   },
   // Après que le composant soit construit
