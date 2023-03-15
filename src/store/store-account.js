7d18a58d4bb1537d255f7a12f17e2b16b46d8481
Chappuis Flavien <75837575+Flavien-dev@users.noreply.github.com>
2023-03-15 11:34:09 +0100

    j'ai commenté le code des fonctions du compte

--- src/store/store-account.js
+++ src/store/store-account.js
@@ -2,11 +2,17 @@
 import { Loading } from 'quasar'
 import { api } from 'boot/axios'
 
+// liste des comptes
 const state = {
   account: []
 }
 
 const getters = {
+  /**
+   * renvoie les comptes
+   * @param state liste des comptes
+   * @returns la liste des comptes
+   */
   getAccount: function (state) {
     return state.account
 
@@ -20,14 +26,26 @@ const getters = {
 }
 
 const mutations = {
+  /**
+   * modifie un compte
+   * @param state liste des comptes
+   * @param newAccount nouveau comptes
+   * @constructor
+   */
   SET_ACCOUNT (state, newAccount) {
     state.account = newAccount
   },
+  /**
+   * modifie le mot de passe d'un compte
+   * @param state liste des comptes
+   * @param payload modification apportée au compte
+   * @constructor
+   */
   MODIFIER_MDP (state, payload) {
-    // Recherche la tâche et retourne sa position dans le tableau, son index
+    // Recherche le compte et retourne sa position dans le tableau, son index
     const index = state.account.findIndex(el => el.id === payload.id)
 
-    // Si une tâche a été trouvée
+    // Si un compte a été trouvée
     if (index !== -1) {
       // Modifie l'objet trouvé avec les nouvelles valeurs
       Object.assign(state.account[index], payload.updates)
@@ -36,9 +54,16 @@ const mutations = {
 }
 
 const actions = {
+  /**
+   * renvoie le compte connecté
+   * @param commit valide les données
+   * @param payload données du compte
+   */
   getAccountApi ({ commit, payload }) {
+    // exécute la requête post
     api.post('/me', payload)
       .then(function (response) {
+        // si ça fonctionne, le programme stocke les données de l'utilisateur et du token
         commit('setUser', response.data.user)
         commit('setToken', response.data.access_token)
       })
@@ -46,9 +71,12 @@ const actions = {
         console.log(error.response)
       })
   },
-  viderAccount ({ commit }) {
-    commit('SET_ACCOUNT', [])
-  },
+  /**
+   * modifie le mot de passe d'un compte
+   * @param commit valide les données
+   * @param rootState données d'authentification de l'utilisateur
+   * @param payload données de l'utilisateur
+   */
   modifierMDP ({ commit, rootState }, payload) {
     Loading.show()
     // Configuration du header avec token
@@ -56,7 +84,7 @@ const actions = {
       headers: { Authorization: 'Bearer ' + rootState.auth.token }
     }
 
-    // modifie une tâche dans l'API
+    // modifie un compte dans l'API
     api.put('/updateme', payload.updates, config)
       .then(function (response) {
         // Affecte au payload les données retrounée par l'API
